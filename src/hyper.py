78a50fb8608969f5abd4e66e34728a124ee7550c
moyanxinxu <wwxy.mail@gmail.com>
2024-07-19 19:14:23 +0800

    refactor code

--- src/hyper.py
+++ src/hyper.py
@@ -1,2 +1,62 @@
 class hp:
-    splited_images_path = "./images/original_images"
+
+    yolo_model_path = "./models/yolov10x_best.pt"
+    layout_model_path = "./models/layout/"
+
+    """pdf2images"""
+    zoom_x = 4
+    zoom_y = 4
+    images_saved_path = "./images/splited/"
+
+    """images2component"""
+    component_path = "./images/component/"
+    ploted_path = "./images/ploted/"
+
+    id2label = {
+        0: "Caption",
+        1: "Footnote",
+        2: "Formula",
+        3: "List-item",
+        4: "Page-footer",
+        5: "Page-header",
+        6: "Picture",
+        7: "Section-header",
+        8: "Table",
+        9: "Text",
+        10: "Title",
+    }
+
+    label2id = {
+        "Caption": 0,
+        "Footnote": 1,
+        "Title": 10,
+        "Formula": 2,
+        "List-item": 3,
+        "Page-footer": 4,
+        "Page-header": 5,
+        "Picture": 6,
+        "Section-header": 7,
+        "Table": 8,
+        "Text": 9,
+    }
+
+    # 尽量使用优美的颜色
+    label2color = {
+        "Caption": (0, 0, 255),
+        "Footnote": (0, 255, 0),
+        "Formula": (255, 0, 0),
+        "List-item": (0, 255, 255),
+        "Page-footer": (255, 255, 0),
+        "Page-header": (255, 0, 255),
+        "Picture": (0, 0, 128),
+        "Section-header": (255, 255, 255),
+        "Table": (128, 128, 128),
+        "Text": (128, 0, 0),
+        "Title": (0, 128, 0),
+    }
+
+    """DataCollator"""
+    max_length = 512
+    cls_token_id = 0
+    unk_token_id = 3
+    eos_token_id = 2
--- src/toolkits.py
+++ src/toolkits.py
@@ -1,9 +1,18 @@
+import os
+from typing import Dict, List
+
 import fitz
 
 from hyper import hp
 
 
-def pdf2img(file_path, zoom_x=4, zoom_y=4):
+def delete_files_in_folder(folder):
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+        os.remove(file_path)
+
+
+def pdf2images(file_path, zoom_x=hp.zoom_x, zoom_y=hp.zoom_y):
     """
     inputs:
         file_path: str
@@ -12,14 +21,12 @@ def pdf2img(file_path, zoom_x=4, zoom_y=4):
             zoom in x direction, default is 4
         zoom_y: float
             zoom in y direction, default is 4
+    example:
+        pdf2images("example.pdf")
     """
 
     with fitz.open(file_path) as pdf:
         for page_num in range(pdf.page_count):
             page = pdf[page_num]
             image = page.get_pixmap(matrix=fitz.Matrix(zoom_x, zoom_y))
-            image.save(hp.splited_images_path + f"page-{page_num+1}.png")
-
-
-if __name__ == "__main__":
-    pdf2img("example.pdf")
+            image.save(hp.images_saved_path + f"{page_num}.png")
